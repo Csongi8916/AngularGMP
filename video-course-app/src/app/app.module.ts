7b77582cd5e4e8f9040dc30469da9854998cb232
kcsongidev <kcsongidev@gmail.com>
2019-10-18 07:56:20 +0200

    Logo and Footer components are added

--- video-course-app/src/app/app.module.ts
+++ video-course-app/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LogoComponent } from './logo/logo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    LogoComponent
   ],
   imports: [
     BrowserModule,
