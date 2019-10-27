d1a28bf81a627fab562cfa3600d3db38cfdb768c
Komár Csongor <kcsongidev@gmail.com>
2019-10-27 15:02:13 +0100

    Refactoring: Logo moved into header component

--- video-course-app/src/app/app.module.ts
+++ video-course-app/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { LogoComponent } from './logo/logo.component';
+import { LogoComponent } from './header/logo/logo.component';
 import { CourseBreadcrumbComponent } from './course-breadcrumb/course-breadcrumb.component';
 import { CourseContainerComponent } from './course-container/course-container.component';
 import { CourseComponent } from './course-container/course/course.component';
