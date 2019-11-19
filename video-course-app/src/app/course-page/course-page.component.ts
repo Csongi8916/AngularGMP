710cfb158aa8be7f5433081850fa0633f514131d
Csongi8916 <38467520+Csongi8916@users.noreply.github.com>
2019-11-19 10:36:19 +0100

    Added is course service with operations and using from component

--- video-course-app/src/app/course-page/course-page.component.ts
+++ video-course-app/src/app/course-page/course-page.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone, ChangeDetectorRef } from '@angular/core';
 
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import { Course } from '../../Entities/Interfaces';
-import { Title } from '@angular/platform-browser';
+import { CourseService } from '../services/course.service';
 
 
 @Component({
@@ -17,54 +17,22 @@ export class CoursePageComponent implements OnInit {
   private addIcon = faPlus;
 
 
-  constructor() {
+  constructor(private courseService: CourseService) {
     console.log('Running: constructor()');
   }
 
   ngOnInit() {
-    this.courses = [
-      {
-        id: 1,
-        title: "Video Course 2. Name tag",
-        duration: 88,
-        creationDate: new Date("2019.11.09"),
-        topRated: true,
-        description: "Learn about where you can find course descriptions, what information they include, how they work, and details about various components of a course description. Course descriptions report information about a university or college's classes. They're published both in course catalogs that outline degree requirements and in course schedules that contain descriptions for all courses offered during a particular semester.",
-        filter:  "",
-        sortable: null
-      },
-      {
-        id: 2,
-        title: "Video Course 3. Name tag",
-        duration: 210,
-        creationDate: new Date("2019.08.01"),
-        topRated: false,
-        description: "Learn about where you can find course descriptions, what information they include, how they work, and details about various components of a course description. Course descriptions report information about a university or college's classes. They're published both in course catalogs that outline degree requirements and in course schedules that contain descriptions for all courses offered during a particular semester.",
-        filter:  "",
-        sortable: null
-      },
-      {
-        id: 3,
-        title: "Video Course 1. Name tag",
-        duration: 45,
-        creationDate: new Date("2019.12.20"),
-        topRated: false,
-        description: "Learn about where you can find course descriptions, what information they include, how they work, and details about various components of a course description. Course descriptions report information about a university or college's classes. They're published both in course catalogs that outline degree requirements and in course schedules that contain descriptions for all courses offered during a particular semester.",
-        filter:  "",
-        sortable: null
-      },
-    ];
-
-    //TODO It is just value copy, there are issues oportunities!!!
-    this.courses.forEach(c => {
-      c.filter = c.title;
-      c.sortable = c.creationDate;
-    });
+    this.getCourses();
     console.log('Running: ngOnInit()');
+  }
+
+  getCourses(): void {
+    this.courses = this.courseService.getCourses();
   }
 
   deleteCourse(event) {
     console.log(event.id);
+    this.courseService.removeCourse(event.id);
   }
 
   onLoadMore() {
--- video-course-app/src/app/services/course.service.ts
+++ video-course-app/src/app/services/course.service.ts
@@ -1,9 +1,103 @@
 import { Injectable } from '@angular/core';
+import { Course } from 'src/Entities/Interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CourseService {
 
-  constructor() { }
+  //TODO Static???
+  courses: Course[];
+
+  constructor() {
+    this.init();
+  }
+
+  private init() {
+    this.courses = [
+      {
+        id: 1,
+        title: "Video Course 2. Name tag",
+        duration: 88,
+        creationDate: new Date("2019.11.09"),
+        topRated: true,
+        description: "Learn about where you can find course descriptions, what information they include, how they work, and details about various components of a course description. Course descriptions report information about a university or college's classes. They're published both in course catalogs that outline degree requirements and in course schedules that contain descriptions for all courses offered during a particular semester.",
+        filter: "",
+        sortable: null
+      },
+      {
+        id: 2,
+        title: "Video Course 3. Name tag",
+        duration: 210,
+        creationDate: new Date("2019.08.01"),
+        topRated: false,
+        description: "Learn about where you can find course descriptions, what information they include, how they work, and details about various components of a course description. Course descriptions report information about a university or college's classes. They're published both in course catalogs that outline degree requirements and in course schedules that contain descriptions for all courses offered during a particular semester.",
+        filter: "",
+        sortable: null
+      },
+      {
+        id: 3,
+        title: "Video Course 1. Name tag",
+        duration: 45,
+        creationDate: new Date("2019.12.20"),
+        topRated: false,
+        description: "Learn about where you can find course descriptions, what information they include, how they work, and details about various components of a course description. Course descriptions report information about a university or college's classes. They're published both in course catalogs that outline degree requirements and in course schedules that contain descriptions for all courses offered during a particular semester.",
+        filter: "",
+        sortable: null
+      },
+    ];
+
+    //TODO It is just value copy, there are issues oportunities!!!
+    this.courses.forEach(c => {
+      c.filter = c.title;
+      c.sortable = c.creationDate;
+    });
+  }
+
+  getCourses(): Course[] {
+    return this.courses;
+  }
+
+  getCourse(id: number): Course {
+    return this.courses.find(course => course.id === id);
+  }
+
+  //Prefer: parameter as entity or primitives?
+  createCourse(course: Course): void {
+    let max: number = 0;
+    this.courses.forEach(course => {
+      if (course.id > max) {
+        max = course.id;
+      }
+    });
+
+    this.courses.push({
+      id: ++max,
+      title: course.title,
+      duration: course.duration,
+      creationDate: course.creationDate,
+      topRated: course.topRated,
+      description: course.description,
+      filter: "",
+      sortable: null
+    });
+  }
+
+  updateCourse(course: Course): void {
+    let originalCourse: Course = this.courses.find(course => course.id === course.id);
+    originalCourse.title = course.title;
+    originalCourse.duration = course.duration;
+    originalCourse.creationDate = course.creationDate;
+    originalCourse.topRated = course.topRated;
+    originalCourse.description = course.description;
+  }
+
+  removeCourse(id: number): void {
+    for (let i = 0; i < this.courses.length; i++) {
+      if (this.courses[i].id === id) {
+        this.courses.splice(i, 1);
+      }
+    }
+  }
+
 }
