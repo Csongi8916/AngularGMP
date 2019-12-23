d58b85382304ab2d783358e2fe9c1776a13b6e4a
Komár Csongor <kcsongidev@gmail.com>
2019-12-23 16:21:49 +0100

    loading block works without issues

--- video-course-app/src/app/core/interceptors/loading-interceptor.ts
+++ video-course-app/src/app/core/interceptors/loading-interceptor.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
 import { MatDialog, MatDialogRef } from '@angular/material';
 import { Observable } from 'rxjs';
-import { AuthServiceService } from '../auth/services/auth-service.service';
 import { LoadingBlockComponent } from '../../shared/components/loading-block/loading-block.component';
 import { CourseService } from 'src/app/course/services/course.service';
 
@@ -11,29 +10,32 @@ import { CourseService } from 'src/app/course/services/course.service';
 export class LoadingInterceptor implements HttpInterceptor {
 
   private dialogRef;
+  private openCount;
+  private closeCount;
 
-  constructor(private courseService: CourseService, public dialog: MatDialog) {}
+  constructor(private courseService: CourseService, public dialog: MatDialog) {
+    this.openCount = 0;
+    this.closeCount = 0;
+  }
 
   intercept(req: HttpRequest<any>, next: HttpHandler):
     Observable<HttpEvent<any>> {
     if (this.dialog.openDialogs.length === 0) {
       this.courseService.pendingState.next(true);
       this.courseService.pendingState.subscribe(state => {
-        if (state) {
+        if (state && this.openCount === 0) {
           this.dialogRef = this.dialog.open(LoadingBlockComponent);
-        } else {
-          debugger;
+          this.openCount++;
+        } else if (!state && this.closeCount === 0) {
           this.dialog.closeAll();
           this.dialogRef.close();
-          if (this.dialogRef) {
-            this.dialogRef.afterAllClosed().subscribe(result => {
-              this.courseService.pendingState.unsubscribe();
-            });
-          }
+          this.closeCount++;
         }
       })
     }
 
+    this.openCount = 0;
+    this.closeCount = 0;
     return next.handle(req);
   }
 }
